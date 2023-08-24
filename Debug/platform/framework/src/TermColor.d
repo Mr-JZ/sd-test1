50dcb45dda52ae90434a894215e075958b230b27
Mr-JZ <jan.zisenis@gmail.com>
2023-08-24 14:05:37 +0200

    added all file for the information

---
+++ Debug/platform/framework/src/TermColor.d
@@ -0,0 +1,4 @@
+platform/framework/src/TermColor.o: \
+ ../platform/framework/src/TermColor.cpp \
+ C:/Users/Jan/gitlab/embedded/sd-jan/platform/framework/inc/TermColor.h
+C:/Users/Jan/gitlab/embedded/sd-jan/platform/framework/inc/TermColor.h:
---
+++ Debug/platform/osSupport/src/Semaphore.d
@@ -0,0 +1,8 @@
+platform/osSupport/src/Semaphore.o: \
+ ../platform/osSupport/src/Semaphore.cpp \
+ C:/Users/Jan/gitlab/embedded/sd-jan/platform/osSupport/inc/Semaphore.h \
+ C:/Users/Jan/gitlab/embedded/sd-jan/platform/framework/inc/utility.h \
+ C:/Users/Jan/gitlab/embedded/sd-jan/platform/osSupport/inc/cmsis_os.h
+C:/Users/Jan/gitlab/embedded/sd-jan/platform/osSupport/inc/Semaphore.h:
+C:/Users/Jan/gitlab/embedded/sd-jan/platform/framework/inc/utility.h:
+C:/Users/Jan/gitlab/embedded/sd-jan/platform/osSupport/inc/cmsis_os.h:
